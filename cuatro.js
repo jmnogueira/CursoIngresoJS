6b7bf81514001fb26f828cd1a89b913994487503
jmnogueira <52511619+jmnogueira@users.noreply.github.com>
2019-07-12 21:45:04 -0300

    Add files via upload

---
+++ cuatro.js
@@ -0,0 +1,35 @@
+function mostrar()
+{
+    var numero1;
+    var numero2;
+    var resultado;
+
+    numero1=prompt(numero1);
+    numero2=prompt(numero2);
+
+    if (numero1==numero2)
+    {
+        numero1=parseInt(numero1);
+        numero2=parseInt(numero2);
+        resultado=numero1+numero2;
+        
+    }
+    else (numero1>numero2);
+    {
+        resta=(numero1-numero2);
+    }
+        if (numero1<numero2)
+        {
+            suma=(numero1+numero2);
+        }
+            else (resultado>10)
+            {
+                alert("la suma es xxx" + " supero el 10"+resultado);
+            }
+
+
+    
+
+
+}
+ 
